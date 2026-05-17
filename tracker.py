058b704c2cc79831d83c6b2148ed1b807634ee50
SipunovD <alena.sipunova@mail.ru>
2026-05-17 22:01:14 +0300

    Добавлен шаблон для меню и функций

---
+++ tracker.py
@@ -0,0 +1,14 @@
+def load_books():
+    pass
+
+def save_books(books):
+    pass
+
+def main():
+    while True:
+        # меню
+        # вызов функции
+        break
+
+if __name__ == "__main__":
+    main()
